e9efd34a428762d0299ac389b7d0d4225009ab29
Pablo Stafforini <pablo@stafforini.com>
2023-08-24 17:33:40 +0200

    Add .dir-locals.el

---
+++ refs/.dir-locals.el
@@ -0,0 +1,3 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+((bibtex-mode . ((eval . (add-hook 'after-save-hook #'ps/bibtex-auto-clean-entry nil t)))))

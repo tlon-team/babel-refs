1516a1f33db6190d8735058193502d9415315d56
Pablo Stafforini <pablo@stafforini.com>
2024-05-09 10:58:25 -0300

    Update .dir-locals.el: update function names

--- bib/.dir-locals.el
+++ bib/.dir-locals.el
@@ -1,3 +1,3 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
-((bibtex-mode . ((eval . (add-hook 'after-save-hook #'tlon-babel-auto-clean-entry nil t)))))
+((bibtex-mode . ((eval . (add-hook 'after-save-hook #'tlon-auto-clean-entry nil t)))))

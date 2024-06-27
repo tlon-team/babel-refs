6b96716b07d93851b9795863852fb1517495f399
Pablo Stafforini <pablo@stafforini.com>
2024-06-27 11:58:51 -0300

    Update .dir-locals.el

--- bib/.dir-locals.el
+++ bib/.dir-locals.el
@@ -1,3 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
-((bibtex-mode . ((eval . (add-hook 'after-save-hook #'tlon-auto-clean-entry nil t)))))
+((bibtex-mode . ((eval . (add-hook 'after-save-hook #'tlon-auto-clean-entry nil t))
+		 (eval . (add-hook 'after-save-hook #'tlon-deepl-fix-encoding-persistent nil t))))
+ (json-mode . ((jinx-languages . "es fr it")
+	       (flycheck-languagetool-language . "es"))))

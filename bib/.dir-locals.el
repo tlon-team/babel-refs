a44bb015e1f63c4f1dfd99d345d54fb5139f4f1e
Pablo Stafforini <pablo@stafforini.com>
2025-04-08 11:05:16 -0300

    Update .dir-locals.el

--- bib/.dir-locals.el
+++ bib/.dir-locals.el
@@ -2,5 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 ((bibtex-mode . ((eval . (add-hook 'after-save-hook #'tlon-auto-clean-entry nil t))
 		 (eval . (add-hook 'after-save-hook #'tlon-deepl-fix-encoding-persistent nil t))))
- (json-mode . ((jinx-languages . "ar es fr it")
+ (json-mode . ((jinx-languages . "ar es fr it ") ; Japanese, Korean not supported by Aspell
 	       (flycheck-languagetool-language . "es"))))

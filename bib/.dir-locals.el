4f3ba1b1879586a389c015a4b8da7455dd479d9a
Pablo Stafforini <pablo@stafforini.com>
2025-05-04 10:08:13 -0300

    Update .dir-locals.el: remove call to obsolete function

--- bib/.dir-locals.el
+++ bib/.dir-locals.el
@@ -1,6 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
-((bibtex-mode . ((eval . (add-hook 'after-save-hook #'tlon-auto-clean-entry nil t))
-		 (eval . (add-hook 'after-save-hook #'tlon-deepl-fix-encoding-persistent nil t))))
+((bibtex-mode . ((eval . (add-hook 'after-save-hook #'tlon-auto-clean-entry nil t))))
  (json-mode . ((jinx-languages . "ar es fr it ") ; Japanese, Korean not supported by Aspell
 	       (flycheck-languagetool-language . "es"))))
